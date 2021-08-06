3dda907e7707043f0c0c0b2485c1a020a35e4c92
bzayn02 <nagarkotibezay02@gmail.com>
2021-08-06 13:50:33 +1000

    Hours calculation updated

--- script.js
+++ script.js
@@ -2,32 +2,46 @@ const taskList = [];
 
 const badTaskList = [];
 
+const hrWeek = 168;
+
+let taskListHrs=0;
+let badTaskListHrs=0;
+
+
 
 
 
 const handleOnSubmit= e => {
     const formData = new FormData(e);
     const task = formData.get("task");
-    const hr = formData.get("hr");
+    const hr = +formData.get("hr");
     const newTask={
         // task:task,
         // hr:hr OR
         task,hr
     };
+
+taskListHrs=taskList.reduce((subTotal,item)=>(subTotal+=item.hr),0);
+if (taskListHrs + hr >hrWeek){
+return alert("Sorry, you do not have enough hours to allocate this task in the list! Try reducing some other tasks.");
+}
+
+document.getElementById("totalHrs").innerText=taskListHrs+hr;
+    
     // console.log(newTask)
 
 //pushing list to the global array
 taskList.push(newTask);
 console.log(taskList)
     displayTaskList();
+  
 };
 
 //displaying teh tasks lists
 const displayTaskList = () =>{
-let tasks = ""
+let tasks = "";
 
     taskList.map((item,i)=>{
-        console.log()
         tasks += `
         <li>
 								<div class="items">
@@ -36,9 +50,9 @@ let tasks = ""
 											>${item.task}</label
 										></span
 									>
-                                    <span class="hrs">${item.hr}hrs/week</span>
+                                    <span class="hrs">${item.hr}hrs/w</span>
                                     <button onclick="markAsNotToDoTask(${i})">Mark Not To Do</button>
-                                    <button onclick="deleteItem()">Delete</button>
+                                    <button onclick="deleteItem(${i})">Delete</button>
 								</div>
 							</li>
         `;
@@ -63,7 +77,7 @@ const displayBadTaskList = () =>{
                                                 >${item.task}</label
                                             ></span
                                         >
-                                        <span class="hrs">${item.hr}hrs/week</span>
+                                        <span class="hrs">${item.hr}hrs/w</span>
                                         
                                         <button onclick="markToDoTask(${i})">Mark To Do</button>
                                     </div>
@@ -74,13 +88,14 @@ const displayBadTaskList = () =>{
     
     
         document.getElementById("bad-task-list").innerHTML = tasks;
+        totalBadHours();
     };
 
 //marks the task as not to do list function
-const markAsNotToDoTask=(i)=>{
+const markAsNotToDoTask=i=>{
     //steps to shift to not to do list
     //1. find out which item is clicked
-    console.log(taskList[i]);
+  
 
     //2. remove that item from the array and put in the variable
 
@@ -94,7 +109,7 @@ const markAsNotToDoTask=(i)=>{
 };
 
 
-const markToDoTask=(i)=>{
+const markToDoTask=i=>{
     //steps to shift to not to do list
     //1. find out which item is clicked
 
@@ -113,5 +128,21 @@ const markToDoTask=(i)=>{
 //deleting items function
 const deleteItem = (i) => {
     taskList.splice(i,1);
+    
     displayTaskList();
+    totalTaskHours();
+};
+
+//calculate bad hours
+
+const totalBadHours = () => {
+    const total = badTaskList.reduce((subTotal, item) => (subTotal += item.hr),0);
+    document.getElementById("totalBadHrs").innerText = total;
+};
+
+
+//calculate total task hours
+const totalTaskHours = () =>{
+const total = taskList.reduce((subTotal,item)=>(subTotal += item.hr),0);
+document.getElementById("totalHrs").innerText=total;
 };
